cf1c6777056bda2ef016e17fbba614b28634c731
shervin nj <nj_shervin@yahoo.com>
2022-04-19 11:29:29 -0500

    astefadeh az preventDefult va EventLissenr ha

---
+++ js.js
@@ -0,0 +1,15 @@
+document.querySelector('.button').addEventListener('click',
+function(e){
+    console.log('hello world');
+    e.preventDefault
+})
+
+//ba estefadeh az addEventListenner ma mitoonim
+//be elemanemoon raftar bedim
+//ma dar mesaleh balah goftim har vaght click kardim 
+//bia dastooreh hello world chap kon 
+//va ziresh az dastooreh preventDefault estefadeh kardim
+//ta zamani ke hello world chap mishe napadidi nashe
+//in dastoor prevent karesh ine ke raftar haye 
+//taghaye tag hayi ke javascript
+//be soorateh pishfarz dareh ro khonsa mikoneh1
