48944a80a060c25d22088ba94a9c2c989d4489bb
shervin nj <nj_shervin@yahoo.com>
2022-04-19 13:45:48 -0500

    estefadeh az Eventlistener ('dblclick',function) barayeh 2 bar click kardan rooyeh harchi

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
-document.querySelector('.button').addEventListener('click',
-function(e){
-    console.log('hello world');
-    e.preventDefault
-})
+// document.querySelector('.button').addEventListener('click',
+// function(e){
+//     console.log('hello world');
+//     e.preventDefault
+// })
 
 //ba estefadeh az addEventListenner ma mitoonim
 //be elemanemoon raftar bedim
@@ -13,3 +13,14 @@ function(e){
 //in dastoor prevent karesh ine ke raftar haye 
 //taghaye tag hayi ke javascript
 //be soorateh pishfarz dareh ro khonsa mikoneh1
+
+
+//+++++++++++++++++++++++++++++++++++++++++++
+document.querySelector('.button').addEventListener('dblclick',function(e){
+    console.log("duble click");
+    e.preventDefault();
+})
+
+// estedeh az tarfand Eventlistner va function barayeh inke
+// 2 bar click konim rooyeh tagemoon ta amaliyat anjam beshe
+// vali dar marhaleyeh aval bayad be jayeh click bezarim dblclick
